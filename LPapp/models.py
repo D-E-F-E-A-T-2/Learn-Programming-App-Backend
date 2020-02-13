a04f3b2f7c162ba42abc08559ae097cb9bb6d4be
root <kushchoudhary8@gmail.com>
2020-02-13 06:40:41 +0530

    user model created

--- LPapp/models.py
+++ LPapp/models.py
@@ -9,8 +9,17 @@ class Users(db.Model):
     API call will interact with Users table
     """
     __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
+    
     username = db.Column(db.String(50))
     password = db.Column(db.String(100)) #bcrypt storage needs 60 size
     email = db.Column(db.String(60), unique=True)
+    
     admin_status = db.Column(db.Boolean, nullable=False, default=0)
+    pro_status = db.Column(db.Boolean, nullable=False, default=0)
+    
+    total_coin = db.Column(db.Integer, nullable=False, default=0)
+    current_coin = db.Column(db.Integer, nullable=False, default=0)
+    spent_coin = db.Column(db.Integer, nullable=False, default=0)
+
